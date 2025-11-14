0b61777069c89dda34a193bb4ec652721aed15ce
morival <mateusz.harlos@gmail.com>
2025-11-14 11:50:04 +0000

    added tesst

--- src/test/java/com/napier/integration/CountryIntegrationTests.java
+++ src/test/java/com/napier/integration/CountryIntegrationTests.java
@@ -1,20 +1,25 @@
 package com.napier.integration;
 import com.napier.sem.App;
 import com.napier.sem.countries.CountryReports;
+import com.napier.sem.tools.DbTools;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+
+import java.sql.SQLException;
+
 public class CountryIntegrationTests {
     static App reportSystem;
     static CountryReports countryReport;
     @BeforeAll
-    static void init()
-    {
+    static void init() throws SQLException, InterruptedException {
+        DbTools.connect();
         reportSystem = new App();
         countryReport = new CountryReports();
     }
     @Test
     void runCountryQueryTest(){
-
+        countryReport.allCountriesInWorldReport();
     }
+
 }
