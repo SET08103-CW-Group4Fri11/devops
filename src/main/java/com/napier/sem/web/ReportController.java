26ebb32f9c778b3cc175492fbd2f56227855f9eb
Morival <mateusz.harlos@gmail.com>
2025-11-24 14:54:28 +0000

    created ReportController file and updated pom. some problem with compiling appearied. to be resolved

---
+++ src/main/java/com/napier/sem/web/ReportController.java
@@ -0,0 +1,20 @@
+// java
+package com.napier.sem.web;
+
+import com.napier.sem.populationReports.PopulationReports;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ReportController {
+
+    private final PopulationReports populationReports = new PopulationReports();
+
+    @GetMapping("/population/continent")
+    public String continentReport(Model model) {
+        String output = populationReports.getContinentPopulationReport();
+        model.addAttribute("reportText", output);
+        return "populationReport";
+    }
+}
