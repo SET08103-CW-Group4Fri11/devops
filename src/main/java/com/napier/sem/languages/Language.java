8482055dac5f12fc0d4538795ca4d43446da9830
Morival <mateusz.harlos@gmail.com>
2025-11-25 23:00:22 +0000

    created Language Class

---
+++ src/main/java/com/napier/sem/languages/Language.java
@@ -0,0 +1,34 @@
+package com.napier.sem.languages;
+
+/**
+ * A class to contain languages
+ */
+public class Language {
+    // Attributes
+    private String language;
+    private int speakers;
+    private double percentage;
+
+    /**
+     * @param language the language name
+     * @param speakers the number of speakers
+     * @param percentage the percentage of speakers
+     */
+    public Language(String language, int speakers, double percentage) {
+        this.language = language;
+        this.speakers = speakers;
+        this.percentage = percentage;
+    }
+
+    // Getters
+    public String getLanguage() {return language;}
+
+    public int getSpeakers() {return speakers;}
+
+    public double getPercentage() {return percentage;}
+
+    @Override
+    public String toString() {
+        return String.format("Language{language='%s', speakers=%d, percentage=%.2f}", language, speakers, percentage);
+    }
+}
