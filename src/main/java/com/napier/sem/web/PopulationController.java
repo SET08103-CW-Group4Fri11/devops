4f892cb7b0f9a757779e7822061f9dd567227c47
Morival <mateusz.harlos@gmail.com>
2025-11-25 17:54:10 +0000

    finished population files

--- src/main/java/com/napier/sem/web/PopulationController.java
+++ src/main/java/com/napier/sem/web/PopulationController.java
@@ -15,10 +15,27 @@ public class PopulationController {
         return "populationReport";
     }
 
-    @GetMapping("/population/all")
+    @GetMapping("/population/continent-cities")
     public String getContinentPopulationReport(Model model) {
         String output = populationReports.getContinentPopulationReport();
-        model.addAttribute("getContinentPopulationReport", output);
+        model.addAttribute("title", "Continent Population Report");
+        model.addAttribute("output", output);
+        return "populationReport";
+    }
+
+    @GetMapping("/population/region-cities")
+    public String getRegionPopulationReport(Model model) {
+        String output = populationReports.getRegionPopulationReport();
+        model.addAttribute("title", "Region Population Report");
+        model.addAttribute("output", output);
+        return "populationReport";
+    }
+
+    @GetMapping("/population/country-cities")
+    public String getCountryPopulationReport(Model model) {
+        String output = populationReports.getCountryPopulationReport();
+        model.addAttribute("title", "Country Population Report");
+        model.addAttribute("output", output);
         return "populationReport";
     }
 }
