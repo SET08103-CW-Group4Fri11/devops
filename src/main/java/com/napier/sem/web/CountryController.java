473845589a8864e875e33ecc2ab032a7678e143c
Morival <mateusz.harlos@gmail.com>
2025-11-25 16:23:10 +0000

    finished controller and html for countries

--- src/main/java/com/napier/sem/web/CountryController.java
+++ src/main/java/com/napier/sem/web/CountryController.java
@@ -4,6 +4,7 @@ import com.napier.sem.countries.CountryReports;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class CountryController {
@@ -15,10 +16,51 @@ public class CountryController {
         return "countryReport";
     }
 
-    @GetMapping("/countries/all")
+    @GetMapping("/countries/all-world")
     public String getAllCountriesInWorldReport(Model model) {
         String output = countryReports.allCountriesInWorldReport();
-        model.addAttribute("getAllCountriesInWorldReport", output);
+        model.addAttribute("title", "All Countries in the World Report");
+        model.addAttribute("output", output);
+        return "countryReport";
+    }
+
+    @GetMapping("/countries/all-continent")
+    public String getAllCountriesInContinentReport(@RequestParam("continent") String continent, Model model) {
+        String output = countryReports.allCountriesInContinentReport(continent);
+        model.addAttribute("title", "All Countries in " + continent + " Continent Report");
+        model.addAttribute("output", output);
+        return "countryReport";
+    }
+
+    @GetMapping("/countries/all-region")
+    public String getAllCountriesInRegionReport(@RequestParam("region") String region, Model model) {
+        String output = countryReports.allCountriesInRegionReport(region);
+        model.addAttribute("title", "All Countries in " + region + " Region Report");
+        model.addAttribute("output", output);
+        return "countryReport";
+    }
+
+    @GetMapping("/countries/n-world")
+    public String getNCountriesInWorldReport(@RequestParam("n-w") int n, Model model) {
+        String output = countryReports.topNCountriesInWorldReport(n);
+        model.addAttribute("title", "Top " + n + " Countries in the World Report");
+        model.addAttribute("output", output);
+        return "countryReport";
+    }
+
+    @GetMapping("/countries/n-continent")
+    public String getNCountriesInContinentReport(@RequestParam("n-c") int n, @RequestParam("continent-n") String continent, Model model) {
+        String output = countryReports.topNCountriesInContinentReport(n, continent);
+        model.addAttribute("title", "Top " + n + " Countries in " + continent + " Continent Report");
+        model.addAttribute("output", output);
+        return "countryReport";
+    }
+
+    @GetMapping("/countries/n-region")
+    public String getNCountriesInRegionReport(@RequestParam("n-r") int n, @RequestParam("region-n") String region, Model model) {
+        String output = countryReports.topNCountriesInRegionReport(n, region);
+        model.addAttribute("title", "Top " + n + " Countries in " + region + " Region Report");
+        model.addAttribute("output", output);
         return "countryReport";
     }
 }
