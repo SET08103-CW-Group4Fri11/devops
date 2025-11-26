3d3720041836469311156a07791db5cd1e049208
Ben Arthurs <ben.arthurs1006@gmail.com>
2025-11-26 12:38:51 +0000

    Created Specific Population Reports Class

---
+++ src/main/java/com/napier/sem/populationReports/SpecificPopulationReports.java
@@ -0,0 +1,51 @@
+package com.napier.sem.populationReports;
+
+import com.napier.sem.tools.DbTools;
+import java.sql.*;
+
+public class SpecificPopulationReports {
+
+    /** private function for running sql queries*/
+    private Long runQuery(String sql, Object... params) throws SQLException {
+        if (DbTools.getCon() == null) {
+            throw new SQLException("No DB connection. Call DbTools.connect() first.");
+        }
+        try (PreparedStatement preStatement = DbTools.getCon().prepareStatement(sql)) {
+            for (int i = 0; i < params.length; i++) {
+                preStatement.setObject(i + 1, params[i]);
+            }
+
+            ResultSet rs = preStatement.executeQuery();
+            if (rs.next()) {
+                return rs.getLong(1);
+            } else {
+                return null;
+            }
+        }
+    }
+    /** This method returns the population of the specified population area and the name of it. */
+    /** Example useage - long pop = getPopulation("city", "Dawson City") */
+    public Long getPopulation(String type, String name) throws SQLException {
+        String sql;
+
+        switch (type.toLowerCase()) {
+            case "world":
+                sql = "SELECT SUM(Population) FROM country";
+                return runQuery(sql);
+            case "continent":
+                sql = "SELECT SUM(Population) FROM country WHERE Continent = ?";
+                return runQuery(sql, name);
+            case "region":
+                sql = "SELECT SUM(Population) FROM country WHERE Region = ?";
+                return runQuery(sql, name);
+            case "country":
+                sql = "SELECT Population FROM country WHERE Name = ?";
+                return runQuery(sql, name);
+            case "city":
+                sql = "SELECT Population FROM city WHERE Name = ?";
+                return runQuery(sql, name);
+            default:
+                throw new IllegalArgumentException("Unknown type: " + type);
+        }
+    }
+}
